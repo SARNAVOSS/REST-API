109812945f1461701d797b6e247a099218bc8a8b
Saptarshi <bhattacharyasaptarshi2001@gmail.com>
2023-02-23 20:19:14 +0530

    Services

--- services/question.service.js
+++ services/question.service.js
@@ -4,10 +4,6 @@ exports.findQuestionInCertificateService = async ({certificate: _id, number: siz
  
  // mongodb aggregation pipeline to find n random questions from a category
 
- // check if size is less than the number of questions in the category
-  // if it is, return the number of questions specified by size
-  // if it is not, return all the questions in the category
-
   const question = await Question.aggregate([
     { $match: { certificate: _id } },
     { $sample: { size: size } }
